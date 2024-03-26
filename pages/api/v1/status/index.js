b2e86fc43b1b35f12d7c0813a437b9de62f44233
riquep <henriquesax@gmail.com>
2024-03-26 12:16:50 +0000

    Adiciona os scripts `.env` e `database.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js"
+
+async function status(request, response) {
+  const res = await database.query('SELECT 1+1 as sum;');
+  console.log(res.rows);
   response.status(200).json("mensagem");
 }
 
